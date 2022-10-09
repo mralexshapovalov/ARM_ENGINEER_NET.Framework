de80b345807d0aaac463a348ba6c99d7a6f9f665
Пользователь <@DESKTOP-03FRQ5P>
2022-10-09 18:20:05 +0300

    Added window Catalog_parts

--- ARM_Engineer/Catalog_Parts/Catalog_parts.Designer.cs
+++ ARM_Engineer/Catalog_Parts/Catalog_parts.Designer.cs
@@ -28,20 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_numbers_of = new System.Windows.Forms.TextBox();
+            this.textBox_supplier = new System.Windows.Forms.TextBox();
+            this.textBox_Cost = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.New_entry = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_to_change = new System.Windows.Forms.Button();
+            this.Button_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 295);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(247, 356);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(184, 20);
+            this.textBox_ID.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(247, 392);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox_numbers_of
+            // 
+            this.textBox_numbers_of.Location = new System.Drawing.Point(247, 428);
+            this.textBox_numbers_of.Name = "textBox_numbers_of";
+            this.textBox_numbers_of.Size = new System.Drawing.Size(184, 20);
+            this.textBox_numbers_of.TabIndex = 3;
+            // 
+            // textBox_supplier
+            // 
+            this.textBox_supplier.Location = new System.Drawing.Point(247, 468);
+            this.textBox_supplier.Name = "textBox_supplier";
+            this.textBox_supplier.Size = new System.Drawing.Size(184, 20);
+            this.textBox_supplier.TabIndex = 4;
+            // 
+            // textBox_Cost
+            // 
+            this.textBox_Cost.Location = new System.Drawing.Point(247, 510);
+            this.textBox_Cost.Name = "textBox_Cost";
+            this.textBox_Cost.Size = new System.Drawing.Size(184, 20);
+            this.textBox_Cost.TabIndex = 5;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(177, 359);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 6;
+            this.ID.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 431);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Колличество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Тип товара";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 471);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Поставщик";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 517);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Цена";
+            // 
+            // New_entry
+            // 
+            this.New_entry.Location = new System.Drawing.Point(563, 346);
+            this.New_entry.Name = "New_entry";
+            this.New_entry.Size = new System.Drawing.Size(114, 38);
+            this.New_entry.TabIndex = 10;
+            this.New_entry.Text = "Новая запись";
+            this.New_entry.UseVisualStyleBackColor = true;
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(563, 392);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(114, 38);
+            this.button_delete.TabIndex = 11;
+            this.button_delete.Text = "Delete";
+            this.button_delete.UseVisualStyleBackColor = true;
+            // 
+            // button_to_change
+            // 
+            this.button_to_change.Location = new System.Drawing.Point(563, 446);
+            this.button_to_change.Name = "button_to_change";
+            this.button_to_change.Size = new System.Drawing.Size(114, 38);
+            this.button_to_change.TabIndex = 12;
+            this.button_to_change.Text = "Изменить";
+            this.button_to_change.UseVisualStyleBackColor = true;
+            // 
+            // Button_save
+            // 
+            this.Button_save.Location = new System.Drawing.Point(563, 492);
+            this.Button_save.Name = "Button_save";
+            this.Button_save.Size = new System.Drawing.Size(114, 38);
+            this.Button_save.TabIndex = 13;
+            this.Button_save.Text = "Сохранить";
+            this.Button_save.UseVisualStyleBackColor = true;
             // 
             // Catalog_parts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.Button_save);
+            this.Controls.Add(this.button_to_change);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.New_entry);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.textBox_Cost);
+            this.Controls.Add(this.textBox_supplier);
+            this.Controls.Add(this.textBox_numbers_of);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox_ID);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Catalog_parts";
             this.Text = "Catalog_parts";
             this.Load += new System.EventHandler(this.Catalog_parts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_numbers_of;
+        private System.Windows.Forms.TextBox textBox_supplier;
+        private System.Windows.Forms.TextBox textBox_Cost;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button New_entry;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_to_change;
+        private System.Windows.Forms.Button Button_save;
     }
 }
